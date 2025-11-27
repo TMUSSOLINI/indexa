13a373bd1212e9a4bb1dcfe489590470cda5ee9f
Thiago Mussolini <tmussolini@apoioprodesp.sp.gov.br>
2025-11-27 19:20:08 -0300

    Separando as responsabilidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './componentes/container/container.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { SeparadorComponent } from './componentes/separador/separador.component';
-import { ContatoComponent } from './componentes/contato/contato.component';
-import { FormsModule } from '@angular/forms';
 import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
-
-interface Contato {
-  id: number;
-  nome: string;
-  telefone: string;
-}
-
-import agenda from '../app/agenda.json';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
 @Component({
   selector: 'app-root',
@@ -22,50 +10,11 @@ import agenda from '../app/agenda.json';
   imports: [
     CommonModule,
     RouterOutlet,
-    ContainerComponent,
-    CabecalhoComponent,
-    SeparadorComponent,
-    ContatoComponent,
-    FormsModule,
     FormularioContatoComponent,
+    ListaContatosComponent,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvxywz';
-  contatos: Contato[] = agenda;
-
-  filtroPorTexto: string = '';
-
-  private removerAcentos(texto: string): string {
-    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  }
-
-  filtrarContatosPorTexto(): Contato[] {
-    if (!this.filtroPorTexto) {
-      return this.contatos;
-    }
-    return this.contatos.filter((contato) => {
-      // Compara os nomes sem acentuações
-      return this.removerAcentos(contato.nome)
-        .toLowerCase()
-        .includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
-    });
-  }
-
-  filtrarContatosPorLetraInicial(letra: string): Contato[] {
-    return this.filtrarContatosPorTexto().filter((contato) => {
-      // Compara a letra inicial sem considerar acentuações
-      return this.removerAcentos(contato.nome)
-        .toLowerCase()
-        .startsWith(this.removerAcentos(letra).toLowerCase());
-    });
-  }
-
-  filtrarContatoPorLetraInicial(letra: string): Contato[] {
-    return this.filtrarContatosPorTexto().filter((contato) => {
-      return contato.nome.toLowerCase().startsWith(letra);
-    });
-  }
-}
+export class AppComponent {}
