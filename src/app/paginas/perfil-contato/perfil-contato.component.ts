4c8f95045b4016fbc51b0f57b866c4cda5f91f09
Thiago Mussolini <tmussolini@apoioprodesp.sp.gov.br>
2025-12-02 18:29:44 -0300

    Salvando um contato no formulario

---
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-perfil-contato',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './perfil-contato.component.html',
+  styleUrl: './perfil-contato.component.css'
+})
+export class PerfilContatoComponent {
+
+}
