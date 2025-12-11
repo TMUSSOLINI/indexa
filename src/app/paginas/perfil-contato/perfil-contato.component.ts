c5503b172fb2f08dcc548fced103b7fa3c49ef9c
Thiago Mussolini <tmussolini@apoioprodesp.sp.gov.br>
2025-12-11 18:29:41 -0300

    Ecibindo foto

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -4,11 +4,12 @@ import { ContainerComponent } from '../../componentes/container/container.compon
 import { Contato } from '../../componentes/contato/contato';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ContatoService } from '../../services/contato.service';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
-  imports: [CommonModule, ContainerComponent, RouterLink],
+  imports: [CommonModule, ContainerComponent, RouterLink, SeparadorComponent],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css',
 })
@@ -16,6 +17,7 @@ export class PerfilContatoComponent implements OnInit {
   contato: Contato = {
     id: 0,
     nome: '',
+    avatar: '',
     telefone: '',
     email: '',
     aniversario: '',
